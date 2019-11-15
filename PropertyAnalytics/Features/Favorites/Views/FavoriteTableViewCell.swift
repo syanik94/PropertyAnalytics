e2e298291dca38645073a3ab54739dea5683c2e2
Yanik <syanik@yahoo.com>
2019-11-15 16:26:57 -0600

    Initial Commit

---
+++ PropertyAnalytics/Features/Favorites/Views/FavoriteTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteTableViewCell.swift
+//  PropertyAnalytics
+//
+//  Created by Yanik Simpson on 11/15/19.
+//  Copyright © 2019 Yanik Simpson. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteTableViewCell: UITableViewCell {
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+}
